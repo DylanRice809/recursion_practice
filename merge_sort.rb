44326462d34b02f9c78d80c77cbe1eb1206eb9e2
Dylan Rice <dylanpatrickrice@gmail.com>
2023-07-07 00:02:43 +0100

    Complete merge sort algorithm

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,12 +1,46 @@
+def split_in_half (array, output_array=[])
+  output_array[0] = array[0..(array.length/2)-1]
+  output_array[1] = array[(array.length/2)..array.length-1]
+  output_array
+end
+
+def merge (array_1, array_2, output_array=[])
+  x = 0
+  y = 0
+  while (x < array_1.length && y < array_2.length)
+    if (array_1[x] < array_2[y])
+      output_array << array_1[x]
+      x += 1
+    else
+      output_array << array_2[y]
+      y += 1
+    end
+  end
+  if (x < y)
+    while (x < array_1.length)
+      output_array << array_1[x]
+      x += 1
+    end
+  else
+    while (y < array_2.length)
+      output_array << array_2[y]
+      y += 1
+    end
+  end
+  output_array
+end
+
 def merge_sort (array)
   if (array.length < 2)
     return array
   else
-    p array[0..((array.length)/2)-1]
-    merge_sort(array[0..((array.length)/2)-1])
-    p array[array.length-(array.length/2)-1..(array.length-1)]
-    merge_sort(array[array.length-(array.length/2)..(array.length-1)])
+    output_array = []
+    one = merge_sort(split_in_half(array)[0])
+    two = merge_sort(split_in_half(array)[1])
   end
+
+  output_array = merge(one, two)
+  output_array
 end
 
-merge_sort([6,2,8,12,3,9,0])
+p merge_sort([8,1,4,5,2,10,3,7,6,9])
